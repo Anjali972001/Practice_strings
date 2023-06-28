f433bb0326943b8cca47ebbba905490162282839
Anjali Kumari <81029259+Anjali972001@users.noreply.github.com>
2023-06-29 00:34:03 +0530

    Hashing

--- src/hashing/hashmap.java
+++ src/hashing/hashmap.java
@@ -1,4 +1,22 @@
 package hashing;
-
+import java.util.*;
 public class hashmap {
+    public static void main(String[] args) {
+        // we will use hashMap where we want to store the pair of Key and values
+       // suppose we want to store the roll no and name in the table the roll number will act as the key as it is
+        // unique in the nature.
+        HashMap<String,Integer> map=new HashMap<>();
+        // Insertion operation.
+        map.put("India",120);
+        map.put("Usa",30);
+        map.put("china",150);
+
+        // to print the map
+        System.out.println(map);
+        // HashMaps are unordered in nature.
+
+        // If we want to update the value then we will do
+        map.put("china",110);
+        System.out.println(map);
+    }
 }
