5d45ebe50cfea9dadb3d4ec397828d5cddf0312f
Anjali Kumari <81029259+Anjali972001@users.noreply.github.com>
2023-06-28 19:41:37 +0530

    Hashing

--- src/hashing/hashSet.java
+++ src/hashing/hashSet.java
@@ -1,5 +1,6 @@
 package hashing;
 import java.util.HashSet;
+import java.util.Iterator;
 public class hashSet {
     public static void main(String[] args) {
         HashSet<Integer> set=new HashSet<>();// creation of the HasHSet
@@ -24,6 +25,30 @@ public class hashSet {
         {
             System.out.println("Does not Contains 1");
         }
+        //Duplicates are not allowed.
+
+
+        // size  of the set is :
+        System.out.println(set.size());
+
+        // Print all the elements of the set
+        System.out.println(set);
+
+        // Iterator in the hashset:
+        Iterator it =set.iterator();
+        // it.next() will return the elements of the set.
+        // This iterator has two important function-->hasNext,next
+        // set is like the black box it can have the not arranged manner.
+        // hasNext return true when it is pointed to either null or any element of the set
+        // else it will return false Or it is pointing to the last element of the set
+        while(it.hasNext())
+        {
+            System.out.print(it.next()+" ");
+        }
+// There is guarantee that iterator will be in the order,it can be any order.
+// hash set is unordered.
+
+
 
     }
 
